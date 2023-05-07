0e4e79fda7707c397153da37ca863ad42f4fa5fc
Daniel Gomez <90294077+DanielGomez294@users.noreply.github.com>
2023-05-07 15:09:03 -0600

    ywy

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -3,7 +3,7 @@ import { AppRouter } from "./router/AppRouter"
 export const JournalApp = () => {
   return (
     <>
-    
+    <h1>Hola</h1>
     <AppRouter />
 
     </>
--- src/jorunal/pages/JournalPage.jsx
+++ src/jorunal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <div>JournalPageuwu</div>
   )
 }
